5e0e3ae193d39526aec0edf032cd98ba6580caed
LuaxY <yann.guineau@voidmx.net>
2018-02-17 12:42:31 +0100

    Dynamic adverts and use serverFile with correct Content-Type (is like sendfile())

--- main.go
+++ main.go
@@ -8,21 +8,33 @@ import (
     "github.com/grafov/m3u8"
     "bufio"
     "os"
+    "time"
+    "math/rand"
 )
+
+var adverts []*m3u8.MediaPlaylist
 
 func main() {
     fmt.Println("HLS Server")
 
+    rand.Seed(time.Now().Unix())
+
+    loadAdvert("./movies/advert1/index.m3u8")
+    loadAdvert("./movies/advert2/index.m3u8")
+    loadAdvert("./movies/advert3/index.m3u8")
+
     router := mux.NewRouter()
-    router.HandleFunc("/movies/index.m3u8", serverVersion).Methods("GET")
-    router.PathPrefix("/").Handler(http.FileServer(http.Dir("./movies/"))) // TODO: use sendfile from nginx
+    router.HandleFunc("/{name:[A-Za-z0-9]+}/index.m3u8", streamPlaylist).Methods("GET")
+    router.HandleFunc("/{name:[A-Za-z0-9]+}/s/{segment:[0-9]+.ts}", streamSegment).Methods("GET")
     log.Fatal(http.ListenAndServe("0.0.0.0:8080", router))
 }
 
-func serverVersion(w http.ResponseWriter, r *http.Request) {
-    movie  := openPlaylist("./movies/film1/index.m3u8")
-    advert := openPlaylist("./movies/advert/index.m3u8")
-
+func streamPlaylist(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    file := fmt.Sprintf("./movies/%s/index.m3u8", vars["name"])
+    //fmt.Println(file)
+    movie  := openPlaylist(file)
+    advert := adverts[random(0, len(adverts))]
     size := movie.Count() + advert.Count()
     playlist, err := m3u8.NewMediaPlaylist(size, size)
 
@@ -37,7 +49,21 @@ func serverVersion(w http.ResponseWriter, r *http.Request) {
 
     playlist.Close()
 
+    w.Header().Set("Content-Type", "application/x-mpegURL")
     w.Write(playlist.Encode().Bytes())
+}
+
+func streamSegment(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    file := fmt.Sprintf("./movies/%s/s/%s", vars["name"], vars["segment"])
+    //fmt.Println(file)
+    w.Header().Set("Content-Type", "video/MP2T")
+    http.ServeFile(w, r, file)
+}
+
+func loadAdvert(file string) {
+    playlist := openPlaylist(file)
+    adverts = append(adverts, playlist)
 }
 
 func openPlaylist(file string) *m3u8.MediaPlaylist {
@@ -70,4 +96,8 @@ func addPlaylist(destination, playlist *m3u8.MediaPlaylist, isFirst bool) {
             destination.AppendSegment(segment)
         }
     }
+}
+
+func random(min, max int) int {
+    return rand.Intn(max - min) + min
 }
